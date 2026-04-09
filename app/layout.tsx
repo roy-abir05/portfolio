f75bdfdb6e61bab07e0f37500a48458722b5a32e
roy-abir05 <royabirdhn@gmail.com>
2026-04-09 19:44:32 +0530

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Poppins, DM_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
